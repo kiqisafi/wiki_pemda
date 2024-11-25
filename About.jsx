4bad01af956d5485490c64eabad16263ac814f3a
kiqisafi <121747478+kiqisafi@users.noreply.github.com>
2024-11-25 08:37:15 +0700

    Add files via upload

---
+++ About.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="wiki-pemda-container">
+      <div className="wiki-pemda-item">
+        <h3 className="blue-text">Prosedur perizinan pengusahaan air tanah</h3>
+        <p className="blue-text">Last updated 6 months ago</p>
+        <p className="blue-text">Last updated 6 months ago</p>
+      </div>
+      <div className="wiki-pemda-item">
+        <h3 className="blue-text">Prosedur perizinan pengusahaan air tanah</h3>
+        <p className="blue-text">Last updated 6 months ago</p>
+      </div>
+      <div className="wiki-pemda-item">
+        <h3 className="blue-text">Prosedur perizinan pengusahaan air tanah</h3>
+        <p className="blue-text">Last updated 6 months ago</p>
+      </div>
+      <div className="wiki-pemda-item">
+        <h3 className="blue-text">Prosedur perizinan pengusahaan air tanah</h3>
+        <p className="blue-text">Last updated 6 months ago</p>
+      </div>
+      <div className="wiki-pemda-item">
+        <h3 className="blue-text">Prosedur perizinan pengusahaan air tanah</h3>
+        <p className="blue-text">Last updated 6 months ago</p>
+      </div>
+      <div className="wiki-pemda-item">
+        <h3 className="blue-text">Prosedur perizinan pengusahaan air tanah</h3>
+        <p className="blue-text">Last updated 6 months ago</p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
